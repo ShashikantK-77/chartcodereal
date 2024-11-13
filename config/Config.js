2f1e1c36cbe503fafa8dab42dfa8a166e820e4cf
shashikant kamthe <shashikantkamthe77@gmail.com>
2024-11-13 19:34:23 +0530

    second commit with my code

--- config/Config.js
+++ config/Config.js
@@ -15,24 +15,46 @@ require('dotenv').config();
 // };
 
 
-module.exports = {
-    development: {
-      username: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-      host: process.env.HOST,
-      port: process.env.PGPORT,
-      dialect: 'postgres',
-      ssl: {
-        require: false, // Ensure SSL is required for cloud databases
-        rejectUnauthorized: false
-      },
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 60000,  // Increase acquire timeout to 60 seconds
-        idle: 10000
-      }
+// module.exports = {
+//     development: {
+//       username: process.env.USER,
+//       password: process.env.PASSWORD,
+//       database: process.env.DATABASE,
+//       host: process.env.HOST,
+//       port: process.env.PGPORT,
+//       dialect: 'postgres',
+//       ssl: {
+//         require: false, // Ensure SSL is required for cloud databases
+//         rejectUnauthorized: false
+//       },
+//       pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 60000,  // Increase acquire timeout to 60 seconds
+//         idle: 10000
+//       }
       
-    }
-  };
+//     }
+//   };
+
+
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: 'postgres',
+  port: process.env.PGPORT,
+  ssl: {
+    require: false, // Adjust based on your needs
+    rejectUnauthorized: false
+  }
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
