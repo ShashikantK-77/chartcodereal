c2fb6799bafeccf5b1afa735c187a89ac5ddafb3
shashikant kamthe <shashikantkamthe77@gmail.com>
2024-12-16 12:52:01 +0530

    added res.setheader

--- Server.js
+++ Server.js
@@ -14,6 +14,12 @@ const jwt = require('jsonwebtoken'); // Import jsonwebtoken for verification
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 
