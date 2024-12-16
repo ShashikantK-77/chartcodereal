f85a1eb853dc25f9ed015783df701f9ec8339a09
shashikant kamthe <shashikantkamthe77@gmail.com>
2024-12-16 14:47:46 +0530

    updated dtabase config file

--- config/database.js
+++ config/database.js
@@ -32,6 +32,36 @@
 
 
 
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DATABASE,
+//   process.env.USER,
+//   process.env.PASSWORD,
+
+//   {
+//     host: process.env.HOST,
+//     port: process.env.PGPORT,
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       // ssl: {
+//       //   require: false,
+//       //   rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
+//       // },
+//     // dialectOptions: {
+//     //   ssl: false // Disable SSL
+  
+//     },
+//   },
+
+//   pool: {
+//     idleTimeoutMillis: 30000,  // Increase this value to 30 seconds (default is 10000ms)
+//     connectionTimeoutMillis: 2000,  // Increase connection timeout to 2 seconds (default is 2000ms)
+//   },
+// );
+
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
@@ -39,7 +69,6 @@ const sequelize = new Sequelize(
   process.env.DATABASE,
   process.env.USER,
   process.env.PASSWORD,
-
   {
     host: process.env.HOST,
     port: process.env.PGPORT,
@@ -49,12 +78,15 @@ const sequelize = new Sequelize(
       //   require: false,
       //   rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
       // },
-    // dialectOptions: {
-    //   ssl: false // Disable SSL
-  
+    },
+    pool: {
+      idleTimeoutMillis: 30000,  // Increase this value to 30 seconds (default is 10000ms)
+      connectionTimeoutMillis: 2000,  // Increase connection timeout to 2 seconds (default is 2000ms)
     },
   }
 );
+
+module.exports = sequelize;
 
 
 
