290bb201c029a39a6c5edb1b947362bf00156f7f
shashikant kamthe <shashikantkamthe77@gmail.com>
2024-12-16 14:37:04 +0530

    updated dtabase config file

--- config/database.js
+++ config/database.js
@@ -1,4 +1,37 @@
-// config/database.js
+// // config/database.js
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DATABASE,
+//   process.env.USER,
+//   process.env.PASSWORD,
+
+//   {
+//     host: process.env.HOST,
+//     port: process.env.PGPORT,
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       ssl: {
+//         require: false,
+//         rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
+//       },
+//     // dialectOptions: {
+//     //   ssl: false // Disable SSL
+  
+//     },
+//   }
+// );
+
+
+
+
+
+// module.exports = sequelize;
+
+
+
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
@@ -12,10 +45,10 @@ const sequelize = new Sequelize(
     port: process.env.PGPORT,
     dialect: 'postgres',
     dialectOptions: {
-      ssl: {
-        require: false,
-        rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
-      },
+      // ssl: {
+      //   require: false,
+      //   rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
+      // },
     // dialectOptions: {
     //   ssl: false // Disable SSL
   
